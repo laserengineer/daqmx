0f3fd7019df255201fa5c518353b260ec764ece5
Yang Yu <yy19@ualberta.ca>
2023-02-13 13:55:24 -0700

    Modified it for Dev1

--- examples/analog_input.py
+++ examples/analog_input.py
@@ -18,9 +18,9 @@ print(daq)
 # the easiest way to get a single sample is to select the analog input
 # attribute on the daq and interrogate its `value` attribute
 print(f'daq.ai0.value: {daq.ai0.value:.3f}V')
-print(f'daq.ai1.value: {daq.ai1.value:.3f}V')
-print(f'daq.ai2.value: {daq.ai2.value:.3f}V')
-print(f'daq.ai3.value: {daq.ai3.value:.3f}V')
+#print(f'daq.ai1.value: {daq.ai1.value:.3f}V')
+#print(f'daq.ai2.value: {daq.ai2.value:.3f}V')
+#print(f'daq.ai3.value: {daq.ai3.value:.3f}V')
 
 # you will start throwing errors if you interrogate
 # inputs that don't exist on the device (uncomment to see!)
@@ -28,7 +28,7 @@ print(f'daq.ai3.value: {daq.ai3.value:.3f}V')
 
 # for more nuanced control over the analog
 # input, we could use the `capture` method
-values = daq.ai1.capture(
+values = daq.ai0.capture(
     sample_count=10, rate=100,
     max_voltage=10.0, min_voltage=-10.0,
     mode='differential', timeout=3.0
@@ -36,11 +36,11 @@ values = daq.ai1.capture(
 print(f'values: {values} V')
 
 # note that the values come back as type `numpy.ndarray`
-print(f'type(values): {type(values)}')
+"""print(f'type(values): {type(values)}')"""
 
 # if you already know your device name, you might be
 # happier going straight to the `AnalogInput` constructor:
-ai0 = AnalogInput(device='Dev3', analog_input='ai0')
+ai0 = AnalogInput(device='Dev1', analog_input='ai0')
 
 # we can do anything that we could have
 # done previously with the daq.aiX
